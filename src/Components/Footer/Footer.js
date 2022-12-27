5e9c97aa2ed4d5b9e5aaa9b1fe49c7835af8f2c4
Jay Damania <jaydamania52@gmail.com>
2022-12-27 23:20:14 +0000

    amending redirections

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -27,29 +27,29 @@ function Footer() {
       <div className="bottom">
         <div>
           <h4>Project</h4>
-          <a href="#">Changelog</a>
-          <a href="#">Status</a>
-          <a href="#">License</a>
-          <a href="#">All Versions</a>
+          <a href="javascript:void(0);">Changelog</a>
+          <a href="javascript:void(0);">Status</a>
+          <a href="javascript:void(0);">License</a>
+          <a href="javascript:void(0);">All Versions</a>
         </div>
         <div>
           <h4>Community</h4>
-          <a href="#">Github</a>
-          <a href="#">Issues</a>
-          <a href="#">Project</a>
-          <a href="#">Twitter</a>
+          <a href="javascript:void(0);">Github</a>
+          <a href="javascript:void(0);">Issues</a>
+          <a href="javascript:void(0);">Project</a>
+          <a href="javascript:void(0);">Twitter</a>
         </div>
         <div>
           <h4>Help</h4>
-          <a href="#">Support</a>
-          <a href="#">Troubleshooting</a>
-          <a href="#">Contact us</a>
+          <a href="javascript:void(0);">Support</a>
+          <a href="javascript:void(0);">Troubleshooting</a>
+          <a href="javascript:void(0);">Contact us</a>
         </div>
         <div>
           <h4>Others</h4>
-          <a href="#">Terms of Service</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">License</a>
+          <a href="javascript:void(0);">Terms of Service</a>
+          <a href="javascript:void(0);">Privacy Policy</a>
+          <a href="javascript:void(0);">License</a>
         </div>
       </div>
     </div>
